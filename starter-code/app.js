df5fa12fcab1aa31b406f7d5cc0569c7f55da45d
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-17 10:06:10 +0100

    Iteration 1,2,3

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,11 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const laundryRoutes = require('./routes/laundry');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+mongoose.connect('mongodb://localhost/uberlaundry');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,8 +31,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'never do your own laundry again',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+app.use((req, res, next) => {
+  if (req.session.currentUser) {
+    res.locals.currentUserInfo = req.session.currentUser;
+    res.locals.isUserLoggedIn = true;
+  } else {
+    res.locals.isUserLoggedIn = false;
+  }
+
+  next();
+});
+
 app.use('/', index);
-app.use('/users', users);
+app.use('/', users);
+app.use('/laundry' laundryRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
